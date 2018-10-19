b08a5042dc7d5331b2b12462a044377aa6fbb161
Vikram Seshadri <vikramseshadri@Vikrams-MacBook-Pro.local>
2018-10-19 17:32:33 -0500

    Are we done?? finished implementation

--- naive_bayes.py
+++ naive_bayes.py
@@ -12,6 +12,7 @@ This is the main entry point for MP4. You should only modify code
 within this file -- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
+import math
 
 def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter):
     """
@@ -30,4 +31,106 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter):
     """
     # TODO: Write your code here
     # return predicted labels of development set
-    return []
+
+
+    len_ham = (train_labels == 0).sum()
+    p_ham = len_ham/len(train_labels)
+
+    len_spam = (train_labels == 1).sum()
+    p_spam = len_spam/len(train_labels)
+
+
+    # init_probs = {}
+    # for w in train_set:
+    #     init_probs[w] = train_set.count(w)/len(train_set)
+
+    total_spam = 0
+    total_ham = 0
+    spam_sum = {}
+    ham_sum = {}
+
+    for e, i in zip(train_set, train_labels):
+        if(i == 1):
+            total_spam += len(e)
+            for w in e:
+                if w not in spam_sum:
+                    spam_sum[w] = 1
+                elif w in spam_sum:
+                    spam_sum[w] += 1
+        elif(i == 0):
+            total_ham += len(e)
+            for w in e:
+                if w not in ham_sum:
+                    ham_sum[w] = 1
+                elif w in ham_sum:
+                    ham_sum[w] += 1
+
+    # print(spam_probs)
+
+
+
+    ##laplace smoothing
+    #SPAM
+    n = sum(spam_sum.values())
+    V = len(spam_sum) + len(ham_sum)
+
+    spam_probs = {k: math.log((spam_sum[k]+smoothing_parameter)/(n+smoothing_parameter*(V+1))) for k in spam_sum}
+
+    # print(spam_probs)
+    # print(sum(spam_probs.values()))
+
+
+
+
+    dev_probs = {}
+
+    for i,l in enumerate(dev_set):
+        dev_probs[i] = 0
+        for w in l:
+            if(w in spam_probs):
+                dev_probs[i] += spam_probs[w]
+            else:
+                dev_probs[i] += math.log((smoothing_parameter)/(n+smoothing_parameter*(V+1)))
+
+        # print(dev_probs[i])
+
+
+
+
+    #HAM
+    n = sum(ham_sum.values())
+    V = len(spam_sum) + len(ham_sum)
+
+    ham_probs = {k: math.log((ham_sum[k]+smoothing_parameter)/(n+smoothing_parameter*(V+1))) for k in ham_sum}
+
+    #print(sum(ham_probs.values()))
+
+
+
+    dev_probs_ham = {}
+
+    for i,l in enumerate(dev_set):
+        dev_probs_ham[i] = 0
+        for w in l:
+            if(w in ham_probs):
+                dev_probs_ham[i] += ham_probs[w]
+            else:
+                dev_probs_ham[i] += math.log((smoothing_parameter)/(n+smoothing_parameter*(V+1)))
+
+        #print(dev_probs_ham[i], dev_probs[i])
+
+
+    dev_labels = []
+    for i in range(len(dev_set)):
+        if(dev_probs_ham[i] > dev_probs[i]):
+            dev_labels.append(0)
+        else:
+            dev_labels.append(1)
+
+
+
+
+
+
+
+    return dev_labels
